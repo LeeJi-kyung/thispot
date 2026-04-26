9c44eff41a0a03bf30a96fa7d013fda29a2ecbd8
0gonge <Challengef0802@gmail.com>
2026-04-26 11:41:27 +0900

    feat: initial commit

---
+++ THISPOT/THISPOT/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  THISPOT
+//
+//  Created by 송여경 on 4/26/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
